8d9ca1965de14c0eb00b2c5a37d84a15ab033104
M.kathiravan <kathiravan12082004@gmail.com>
2026-02-15 14:53:55 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const dashboardController = require("../controllers/dashboardController");
-
-router.get("/sidebar", dashboardController.getSidebar);
-router.get("/feed", dashboardController.getFeed);
-
-module.exports = router;
