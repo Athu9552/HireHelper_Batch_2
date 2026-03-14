8104a937625caba0be0a0ad740ff9d92f7403e5c
Atharva kumbhar <atharvakumbhar9552@gmail.com>
2026-03-14 21:13:52 +0530

    fix: add email debug logging

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -12,12 +12,21 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, subject, text) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text
-  });
+  console.log('Attempting to send email to:', to);
+  console.log('EMAIL_USER:', process.env.EMAIL_USER);
+  console.log('EMAIL_PASS set:', !!process.env.EMAIL_PASS);
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text
+    });
+    console.log('Email sent successfully:', info.response);
+  } catch (err) {
+    console.error('Email error:', err.message);
+    throw err;
+  }
 };
 
 module.exports = sendEmail;
